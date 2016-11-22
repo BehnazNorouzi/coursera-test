e51abc3998a2fd42a9bd95b0161af45a0b1ba71c
Behnaz <BehnazNorouzi@users.noreply.github.com>
2016-11-23 00:54:44 +0200

    Modified files

--- site/assignments/module4-solution/js/SpeakGoodBye.js
+++ site/assignments/module4-solution/js/SpeakGoodBye.js
@@ -11,17 +11,16 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-(function (names) {
+(function (window) {
 	var speakWord = "Good Bye";
 	var byeSpeaker = {};
-	helloSpeaker.name = names[0];
 
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 	byeSpeaker.speak = function (name) {
-  	console.log(speakWord + " " + byeSpeaker.name);
+  	console.log(speakWord + " " + name);
 }
 	window.byeSpeaker = byeSpeaker;
 
